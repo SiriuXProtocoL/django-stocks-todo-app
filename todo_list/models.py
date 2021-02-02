92ed33ca51023c4c9f6612e721f8bca26a59bedb
SiriuXProtocoL <bebinkrajusalkara@gmail.com>
2021-02-02 17:37:22 +0530

    Database and functionalities

--- todo_list/models.py
+++ todo_list/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+#Step 1: Creating the database, defining the model
+class List(models.Model):
+    item = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
+
+    #to see items in a better way
+    def __str__(self):
+        return self.item
+
+#step 2: Create a migration
+#/usr/bin/python3 /home/siriux/Documents/Project/Django/todo/manage.py makemigrations
+
+#step 3: Push the migration
+#/usr/bin/python3 /home/siriux/Documents/Project/Django/todo/manage.py migrate
+
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
+from .models import List
+from .forms import ListForm
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html', {})
+    if request.method == 'POST':
+        form = ListForm(request.POST or None)
+
+        if form.is_valid():
+            form.save()
+            all_items = List.objects.all
+            messages.success(request, ("Reminder Added to your List!"))
+            return render(request, 'home.html', {'all_items': all_items})
+    
+    else:
+        #pull objects from database
+        all_items = List.objects.all
+        return render(request, 'home.html', {'all_items': all_items})
 
 def about(request):
     return render(request, 'about.html', {})
